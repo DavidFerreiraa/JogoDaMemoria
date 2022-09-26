ca4a426e6d5e390dd5738fadfb396596038f3ac1
David <dfl8@aluno.ifal.edu.br>
2022-09-26 17:13:54 -0300

    Atualização

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,7 +1,7 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { StyleSheet, TouchableOpacity, View } from "react-native";
 
-let abertas = [];
+let abertas = []
 
 export default function Card() {
   let [pontuacao, setPontuacao] = useState(0);
@@ -9,14 +9,14 @@ export default function Card() {
   const [board, setBoard] = useState([
       { status: 0, color: "#3498DB", key: 1},
       { status: 0, color: "#16A085", key: 2},
-      { status: 0, color: "#8E44AD", key: 3},
+      { status: 0, color: "#8A00AD", key: 3},
       { status: 0, color: "#16A085", key: 4},
       { status: 0, color: "#3498DB", key: 5},
-      { status: 0, color: "#8E44AD", key: 6},
+      { status: 0, color: "#8A00AD", key: 6},
       { status: 0, color: "#FE2E2E", key: 7},
       { status: 0, color: "#00FF00", key: 8},
       { status: 0, color: "#9A2EFE", key: 9},
-      { status: 0, color: "#FE9A2E", key: 10},
+      { status: 0, color: "#FE2E2E", key: 10},
       { status: 0, color: "#00FF00", key: 11},
       { status: 0, color: "#9A2EFE", key: 12},
     ]);
@@ -29,18 +29,19 @@ export default function Card() {
       newBoard[index] = item;
       setBoard(newBoard);
       abertas.push(item)
+      console.log(abertas)
       if (abertas.length === 2) {
-        comparaCarta(abertas);
-        abertas = []
+        comparaCarta(abertas)
       }
     }
   };
   
   const comparaCarta = (cards) => {
-    cards[0].color === cards[1].color ? contarPontuacao() : limpaCarta(cards)
+    cards[0].color === cards[1].color ? contarPontuacao() : setTimeout(() => limpaCarta(cards), 1000), console.log("iniciou")
   }
 
   const limpaCarta = (cards) => {
+    console.log("Executou")
     cards.map((card) => {
       card.status = 0
     })
